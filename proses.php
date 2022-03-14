4a4c3145b7a180edabff5bb0a99122b462060430
shafrinakhosyaqolba <100253621+shafrinakhosyaqolba@users.noreply.github.com>
2022-03-14 13:10:12 +0700

    Initial commit

---
+++ proses.php
@@ -0,0 +1,14 @@
+<?php
+$proses = $_GET['proses']
+$nama_siswa = $_GET['nama_siswa'];
+$nilai_uts = $_GET['nilai_uts'];
+$nilaiuas = $_GET['nilai_uas'];
+$nilai_tugas = $_GET['nilai_tugas'];
+$mata_kuliah = $_GET['mata_kuliah'];
+
+echo '<br/> :proses '.$proses;
+echo '<br/> :Nama Siswa '.$nama_siswa;
+echo '<br/> :Nilai UTS '.$nilai_uas;
+echo '<br/> :Nilai Tugas/Praktikum: '.$nilai_tugas;
+echo '<br/> :Mata Kuliah : '.$matkul;
+?>
